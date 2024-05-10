bce767c658b9dde1ffad0b4e5cd41c02a218f1e8
Brooks Stockley <bstockley26@gmail.com>
2024-05-10 11:48:02 -0400

    finished animal planet

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -7,27 +7,41 @@ namespace Inheritance
         static void Main(string[] args)
         {
             // TODO Be sure to follow best practice when creating your classes
+            var emperiorPenguin = new Bird();
+            emperiorPenguin.CallSound = "eeeghhhh";
+            emperiorPenguin.Color = "Black and white";
+            emperiorPenguin.CurvedOrSraightBeak = "Straight";
+            emperiorPenguin.FlyOrFlightless = "Flightless";
+            emperiorPenguin.GroupName = "Colony";
+            emperiorPenguin.LandOrSea = "Land";
+            emperiorPenguin.MovementType = "walk";
+            emperiorPenguin.PredatorOrPrey = "prey";
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
-
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
+            Console.WriteLine($"On this episode of Animal Planet we are focusing the Emperior Penguin." +
+                $"These beutiful animals are {emperiorPenguin.LandOrSea} animals who {emperiorPenguin.MovementType}." +
+                $"You can see these {emperiorPenguin.Color} birds as they are together in their {emperiorPenguin.GroupName}." +
+                $"These birds are {emperiorPenguin.FlyOrFlightless} and use thei {emperiorPenguin.CurvedOrSraightBeak} beak to catch fish." +
+                $"You can hear these birds with their distinct {emperiorPenguin.CallSound} on {emperiorPenguin.LandOrSea}");
 
 
 
 
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
+
+            var turtle = new Reptile();
+
+            turtle.Color = "green";
+            turtle.EctoOrEndo = "EctoMorph";
+            turtle.EggOrNo = "Eggs";
+            turtle.GroupName = "Nest";
+            turtle.LandOrSea = "Semi-Aquatic";
+            turtle.LegsOrNo = "Legs";
+            turtle.MovementType = "walk and swim";
+            turtle.ShedOrNo = "do not shed";
+
+            Console.WriteLine($"On this very special episode of Animal Planet. We are focusing on the Turtle." +
+                $"This marvelous animal is a {turtle.LandOrSea} animal that uses it {turtle.LegsOrNo} to {turtle.MovementType}." +
+                $"We see here multiple turtles together called a {turtle.GroupName} while they protect their {turtle.EggOrNo}." +
+                $"These beutiful animals {turtle.ShedOrNo} thier {turtle.Color} skin");
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
